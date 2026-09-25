20754b2ef48a58a539b9e3668e5f2cfdb44f65b1
vany <leiladvany02@gmail.com>
2026-09-25 14:31:06 +0700

    update tampilan checkout_sukses

--- toko-rajut/checkout_sukses.php
+++ toko-rajut/checkout_sukses.php
@@ -69,16 +69,18 @@ require __DIR__ . '/includes/header.php';
       </div>
     </div>
 
-    <p class="checkout-note checkout-sukses-note">Simpan kode pesanan di atas ya. Admin Lalunaco akan menghubungi kamu lewat WhatsApp di nomor <strong><?= h($transaksi['telepon']) ?></strong> untuk konfirmasi ongkos kirim dan pembayaran.</p>
+    <p class="checkout-note checkout-sukses-note">Simpan kode pesanan di atas untuk melihat detail pesanan. Admin Lalunaco akan menghubungi Anda melalui WhatsApp terkait informasi dan proses pengiriman pesanan.</p>
 
     <div class="checkout-sukses-actions">
       <a class="icon-btn icon-btn-wa" href="<?= h($linkWaKonfirmasi) ?>" target="_blank" rel="noopener">
         <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M17.6 6.3A8.9 8.9 0 0 0 12 4a8.9 8.9 0 0 0-7.8 13.4L3 21l3.7-1.2A8.9 8.9 0 0 0 12 21a8.9 8.9 0 0 0 5.6-15.7zM12 19.3a7.3 7.3 0 0 1-3.9-1.1l-.3-.2-2.6.9.8-2.5-.2-.3A7.3 7.3 0 1 1 19.3 12 7.3 7.3 0 0 1 12 19.3z"/></svg>
         <span>Konfirmasi via WhatsApp</span>
       </a>
-      <a href="invoice.php?kode=<?= urlencode($transaksi['kode']) ?>" target="_blank" class="btn btn-outline">🖨 Cetak Invoice</a>
-      <a href="produk.php" class="btn btn-primary">Lanjut Belanja</a>
-      <a href="index.php" class="btn btn-outline">Kembali ke Beranda</a>
+      <div class="checkout-sukses-row">
+        <a href="invoice.php?kode=<?= urlencode($transaksi['kode']) ?>" target="_blank" class="btn btn-outline">🖨 Cetak Invoice</a>
+        <a href="produk.php" class="btn btn-primary">Lanjut Belanja</a>
+      </div>
+      <a href="index.php" class="checkout-sukses-beranda">Kembali ke Beranda</a>
     </div>
   </div>
 </section>
